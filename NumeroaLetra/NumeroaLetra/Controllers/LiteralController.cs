0ea2dd1a3a23e1835e3dbb82ca9e58b2c69dfce8
Alejandro Sivila <a2019116316@estudiantes.upsa.edu.bo>
2020-09-03 00:24:50 -0400

    conversor de numeros a letras casi terminado, solo falta decorar

---
+++ NumeroaLetra/NumeroaLetra/Controllers/LiteralController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NumeroaLetra.Helpers;
+
+namespace NumeroaLetra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LiteralController : ControllerBase
+    {
+        [HttpGet]
+        public string Literalget(int num) {
+            return (Conversores.NumeroALetras(num));
+        }
+
+        [HttpPost]
+        public string Literalpost([FromHeader] int num)
+        {
+            return (Conversores.NumeroALetras(num));
+        }
+    }
+}
